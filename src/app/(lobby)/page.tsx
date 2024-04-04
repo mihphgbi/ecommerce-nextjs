2c4c274e4b49c68fa3560bcf02c01cfc2dd5d05e
anna <anna.bui@pal.net.vn>
2024-04-04 15:54:32 +0700

    feat navigate bar

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,10 +1,47 @@
-import {Breadcrumb, Button, Layout, Menu} from "antd";
+import {Breadcrumb, Button, Layout, Menu, MenuProps} from "antd";
 import {Content, Header} from "antd/lib/layout/layout";
 import BreadcrumbItem from "antd/lib/breadcrumb/BreadcrumbItem";
-const items = new Array(3).fill(null).map((_, index) => ({
-    key: String(index + 1),
-    label: `nav ${index + 1}`,
-}));
+import {DingdingOutlined} from "@ant-design/icons";
+import React from "react";
+import '../(lobby)/style.scss';
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key?: React.Key | null,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Navigation One', 'sub1',null,[
+        getItem('Item 1', null, null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+        getItem('Item 2', null, null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+    ]),
+
+    getItem('Navigation Two', 'sub2', null,[
+        getItem('Option 5', '5'),
+        getItem('Option 6', '6'),
+        getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+    ]),
+
+    getItem('Navigation Three', 'sub4',null, [
+        getItem('Option 9', '9'),
+        getItem('Option 10', '10'),
+        getItem('Option 11', '11'),
+        getItem('Option 12', '12'),
+    ]),
+];
+
 export default function Lobby() {
     return (
         <Layout>
@@ -16,9 +53,11 @@ export default function Lobby() {
                 display: 'flex',
                 alignItems: 'center',
             }}>
-                <div className="demo-logo flex-none w-[100px] h-[45px] bg-white"></div>
+                <div className="demo-logo flex-none w-[100px] h-[45px]">
+                    <DingdingOutlined style={{ fontSize: '50px', color: '#CED1D6' }}/>
+                </div>
                 <div className="nav-bar grow">
-                    <Menu mode='horizontal' defaultSelectedKeys={['2']} items={items}/>
+                    <Menu mode='horizontal' items={items} style={{background:'none'}}/>
                 </div>
                 <div className="search-box flex-none">
 
