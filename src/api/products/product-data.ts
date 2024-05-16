b8abfd9a56d5d2c8f40a8c5f349619f5de4ff885
anna <anna.bui@pal.net.vn>
2024-05-16 17:04:16 +0700

    Build user page

---
+++ src/api/products/product-data.ts
@@ -0,0 +1,50 @@
+import ProductItem from "@/assets/pre-product-img.png";
+
+export const ProductData = [
+    {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: true,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    },
+    {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: false,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    },
+    {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: true,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    },
+    {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: false,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    }, {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: false,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    }, {
+        image: ProductItem,
+        name: 'Kicks & Carriers',
+        isDiscount: false,
+        description: 'An eco-friendly, reusable bottle designed for daily use and sustainable hydration.',
+        salePrice: '$129.99',
+        price: '$189.99',
+    },
+]
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 import React from "react";
-import {Checkbox, Col, Divider, Input, Row, Switch, Typography} from "antd";
+import {Badge, Button, Checkbox, Divider, Input, List, Switch, Typography} from "antd";
 import Title from "antd/es/typography/Title";
-
+import {HeartOutlined} from "@ant-design/icons";
+import {ProductData} from "@/api/products/product-data";
+import Item from "antd/es/list/Item";
+import '../../style/var_map.scss';
 export default function User() {
     const {Text} = Typography;
     const onChange = () => {
@@ -11,7 +14,8 @@ export default function User() {
     return (
         <>
             <div className={'p-[48px] flex gap-[48px]'}>
-                <div className={'w-[253px] p-[16px] border-solid border-[1px] border-gray-200 rounded-[8px] gap-[16px] flex flex-col flex-wrap'}>
+                <div
+                    className={'min-w-[253px] p-[16px] border-solid border-[1px] border-gray-200 rounded-[8px] gap-[16px] flex flex-col flex-wrap'}>
                     <div>
                         <Title level={4} className={'mb-0'}>Filter by:</Title>
                     </div>
@@ -68,19 +72,78 @@ export default function User() {
                     </div>
                 </div>
                 <div>
-                    <Row gutter={24}>
-                        <Col>
-                            <div>
-                                <div className={'w-[250px] h-[300px] bg-black'}>
+                    <List
+                        bordered={false}
+                        split={false}
+                        itemLayout="horizontal"
+                        grid={{
+                            gutter: 24,
+                            xs: 1,
+                            sm: 2,
+                            md: 4,
+                            lg: 4,
+                            xl: 4,
+                            xxl: 3,
+                        }}
+                        dataSource={ProductData}
+                        renderItem={(item) => (
+                            <Item className={'mbe-0'}>
+                                {
+                                    item.isDiscount ? (
+                                        // <Space direction="vertical" size="middle" style={{width: '100%'}}>
+                                        <Badge.Ribbon text="Discount" color="primary">
+                                            <div>
+                                                <div className={'w-[100%] h-[278px] bg-black'}>
 
-                                </div>
-                                <div className={'p-[16px]'}>
-                                    <Text strong>GreenBottle</Text>
-                                    <Text>An eco-friendly, reusable bottle designed for daily use and sustainable hydration.</Text>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
+                                                </div>
+                                                <div className={'p-[16px] border-solid border-[1px] border-gray-200'}>
+                                                    <div className={'pb-[4px]'}>
+                                                        <Text strong>{item.name}</Text>
+                                                    </div>
+                                                    <div className={'pb-[8px]'}>
+                                                        <Text>{item.description}</Text>
+                                                    </div>
+                                                    <div className={'flex gap-[4px] pb-[16px]'}>
+                                                        <Text strong>{item.salePrice}</Text>
+                                                        <Text delete>{item.price}</Text>
+                                                    </div>
+                                                    <div className={'flex gap-[8px]'}>
+                                                        <Button className={'py-0 w-[200px]'}>Add to cart</Button>
+                                                        <Button icon={<HeartOutlined/>}></Button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </Badge.Ribbon>
+                                        // </Space>
+                                    ) : (
+                                        <div>
+                                            <div className={'w-[100%] h-[278px] bg-black'}>
+
+                                            </div>
+                                            <div className={'p-[16px] border-solid border-[1px] border-gray-200'}>
+                                                <div className={'pb-[4px]'}>
+                                                    <Text strong>{item.name}</Text>
+                                                </div>
+                                                <div className={'pb-[8px]'}>
+                                                    <Text>{item.description}</Text>
+                                                </div>
+                                                <div className={'flex gap-[4px] pb-[16px]'}>
+                                                    <Text strong>{item.price}</Text>
+                                                </div>
+                                                <div className={'flex gap-[8px]'}>
+                                                    <Button className={'py-0 w-[200px]'}>Add to cart</Button>
+                                                    <Button icon={<HeartOutlined/>}></Button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </Item>
+                        )}
+                    />
+                    <div className={'mt-[24px] text-center'}>
+                        <Button className={'py-0 w-[200px]'}>Load more products</Button>
+                    </div>
                 </div>
             </div>
         </>
