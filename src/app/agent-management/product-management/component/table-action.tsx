272ff2eb48bbba5702a4e6001d979877eee6f117
anna <anna.bui@pal.net.vn>
2024-06-19 13:44:57 +0700

    build edit and delete product dialog

--- src/app/agent-management/product-management/component/table-action.tsx
+++ src/app/agent-management/product-management/component/table-action.tsx
@@ -1,15 +1,70 @@
 'use client';
 import EditProduct from "@/app/agent-management/product-management/dialog/edit-product";
 import DeleteProduct from "@/app/agent-management/product-management/dialog/delete-product";
-import {Space} from "antd";
+import {Space, Table, TableProps} from "antd";
 
-const TableAction = () => {
+interface ListProduct {
+    id: string;
+    name: string;
+    price: number;
+    quality: number;
+    description: string;
+    is_sale: boolean;
+    sale_price: number;
+    image: string;
+}
+
+
+const columns: TableProps<ListProduct>['columns'] = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+    },
+    {
+        title: 'Quality',
+        dataIndex: 'quality',
+        key: 'quality',
+    },
+    {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+    },
+    {
+        title: 'Image',
+        dataIndex: 'image',
+        key: 'image',
+    },
+    {
+        title: 'Sale',
+        dataIndex: 'is_sale',
+        key: 'is_sale',
+    },
+    {
+        title: 'Sale Price',
+        dataIndex: 'sale_price',
+        key: 'sale_price',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_,record) => {
+            <Space size="middle">
+                <EditProduct/>
+                <DeleteProduct id={record.id}/>
+            </Space>
+        }
+    }
+];
+const ProductListTable = ({data}) => {
     return (
-        <Space size="middle">
-            <EditProduct/>
-            <DeleteProduct id={'111111'}/>
-        </Space>
-
+        <Table columns={columns} dataSource={data}/>
     )
 }
-export default TableAction
+export default ProductListTable
