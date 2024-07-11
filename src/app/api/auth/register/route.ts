6d22ff441c75085a02cf0e8b58dc366024f79e22
anna <anna.bui@pal.net.vn>
2024-07-11 11:04:12 +0700

    throw exception of post function

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -8,27 +8,29 @@ export async function POST(req: Request ) {
         const body = await req.json()
         if (!PASSWORD_REGEX.test(body.password)) {
             console.log("===========fail");
-            return new NextResponse({status: 401},{data: {status: 'ok'}})
-        } else {
-            const parseData = {
-                email: body.email,
-                username: body.username,
-                password: await bcrypt.hash(body.password, 10),
-                phone: body.password.toString(),
-                address: body.address,
-                is_agent: body.isSeller,
-                is_authenticate: false,
-                full_name:body.fullName.toString()
-            }
-            console.log("==============",parseData)
-            await prisma.user.create({data:parseData})
-            return new NextResponse({status: 202},{data: {status: 'ok'}})
+            throw new Error('Password does not meet criteria')
         }
+        // kiểm tra username đã có chưa
+        // kiểm tra email đã có chưa
+        const parseData = {
+            email: body.email,
+            username: body.username,
+            password: await bcrypt.hash(body.password, 10),
+            phone: body.password.toString(),
+            address: body.address,
+            is_agent: body.isSeller,
+            is_authenticate: false,
+            full_name:body.fullName.toString()
+        }
+        console.log("==============",parseData)
+        await prisma.user.create({data:parseData})
+        return new NextResponse({status: 202},{data: {status: 'ok'}})
+
     } catch (error) {
         console.log("===========",error)
         return new NextResponse(
-            {error: 'Internal server error - delete '},
-            {status: 500}
+            { error: error.message }, // Return the error message thrown
+            { status: 400 } // Use appropriate status code based on the error
         );
     }
 }
